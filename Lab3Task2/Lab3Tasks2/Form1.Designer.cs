07af9bf78a79a526a77f82c1148b154be2d888ed
f1b <33136217+f1b@users.noreply.github.com>
2022-10-05 00:32:03 +0300

    Fruit ninja! (Task 2). I hope, done.

---
+++ Lab3Task2/Lab3Tasks2/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿
+namespace Lab3Tasks2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.Control;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 41);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(952, 449);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseClick);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(96, 12);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(43, 17);
+            this.checkBox1.TabIndex = 2;
+            this.checkBox1.Text = "Wu";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Checked = true;
+            this.checkBox2.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox2.Location = new System.Drawing.Point(11, 12);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(79, 17);
+            this.checkBox2.TabIndex = 3;
+            this.checkBox2.Text = "Bresenham";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            this.checkBox2.CheckedChanged += new System.EventHandler(this.checkBox2_CheckedChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(889, 6);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Wipe out";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.ClientSize = new System.Drawing.Size(976, 502);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.Text = "Fruit ninja!";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Lab3Task2/Lab3Tasks2/Form1.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Data;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Lab3Tasks2
+{
+    public partial class Form1 : Form
+    {
+
+        bool isDrawing = false;
+        Point p;
+
+        public Form1()
+        {
+            InitializeComponent();
+            pictureBox1.Image = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void Bresenham(int x1, int y1, int x2, int y2, System.Drawing.Pen pen)
+        {
+            var pic = (pictureBox1.Image as Bitmap);
+            int dirx, diry;
+            var deltay = Math.Abs(y2 - y1);
+            var deltax = Math.Abs(x2 - x1);
+
+            if (x1 < x2)
+            {
+                dirx = 1;
+            }
+            else
+            {
+                dirx = -1;
+            };
+
+            if (y1 < y2)
+            {
+                diry = 1;
+            }
+            else
+            {
+                diry = -1;
+            };
+
+            int error = deltax - deltay;
+
+            pic.SetPixel(x2, y2, pen.Color);
+            pictureBox1.Invalidate();
+            while (x1 != x2 || y1 != y2)
+            {
+                pic.SetPixel(x1, y1, pen.Color);
+                pictureBox1.Invalidate();
+
+                int error2 = error * 2;
+
+                if (error2 > -deltay)
+                {
+                    x1 += dirx;
+                    error -= deltay;
+                }
+                if (error2 < deltax)
+                {
+                    y1 += diry;
+                    error += deltax;
+                }
+            }
+        }
+
+        private void Wu(int x1, int y1, int x2, int y2, System.Drawing.Pen pen)
+        {
+            float deltax = x2 - x1; float deltay = y2 - y1;
+            var pic = (pictureBox1.Image as Bitmap);
+            if (Math.Abs(deltax) > Math.Abs(deltay))
+            {
+                if (x1 > x2)
+                {
+                    (x1, x2) = (x2, x1);
+                    (y1, y2) = (y2, y1);
+                }
+                pic.SetPixel(x2, y2, pen.Color);
+                pictureBox1.Invalidate();
+
+                float gradient = deltay / deltax;
+                float y = y1 + gradient;
+                for (var x = x1 + 1; x <= x2 - 1; x++)
+                {
+                    pic.SetPixel(x, (int)y, Color.FromArgb((int)((1 - (y - (int)y)) * 255), pen.Color.R, pen.Color.G, pen.Color.B));
+                    pic.SetPixel(x, (int)y + 1, Color.FromArgb((int)((y - (int)y) * 255), pen.Color.R, pen.Color.G, pen.Color.B));
+                    pictureBox1.Invalidate();
+                    y += gradient;
+                }
+            }
+            else
+            {
+                if (y1 > y2)
+                {
+                    (x1, x2) = (x2, x1);
+                    (y1, y2) = (y2, y1);
+                }
+
+                pic.SetPixel(x2, y2, pen.Color);
+                pictureBox1.Invalidate();
+
+                float gradient = deltax / deltay;
+                float x = x1 + gradient;
+
+                for (var y = y1 + 1; y <= y2 - 1; y++)
+                {
+                    pic.SetPixel((int)x, y, Color.FromArgb((int)((1 - (x - (int)x)) * 255), pen.Color.R, pen.Color.G, pen.Color.B));
+                    pic.SetPixel((int)x + 1, y, Color.FromArgb((int)((x - (int)x) * 255), pen.Color.R, pen.Color.G, pen.Color.B));
+                    pictureBox1.Invalidate();
+                    x += gradient;
+                }
+            }
+        }
+
+        private void pictureBox1_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (isDrawing)
+            {
+                if (checkBox1.Checked)
+                {
+                    Wu(p.X, p.Y, e.X, e.Y, Pens.Yellow);
+                }
+                else
+                {
+                    Bresenham(p.X, p.Y, e.X, e.Y, Pens.Blue);
+                }
+            }
+            else
+            {
+                p = e.Location;
+            }
+
+            isDrawing = !isDrawing;
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            checkBox2.Checked = !checkBox1.Checked;
+        }
+
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+            checkBox1.Checked = !checkBox2.Checked;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Image = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+        }
+
+    }
+}
